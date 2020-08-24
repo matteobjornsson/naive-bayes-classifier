3ef515dd84f600b2c2dfaf723e02b3aa32a87baa
Nicholas Stone <stonen2@xavier.edu>
2020-08-24 15:40:30 -0600

    Glass Data for upload

--- Driver.py
+++ Driver.py
@@ -8,10 +8,28 @@
 #
 ##################################################################### MODULE COMMENTS #####################################################################
 
+import pandas as pd 
+import os 
 
 
 def main(): 
     print("Program Starting")
+    """
+    Read in the data files 
+    """
+    #Create a data frame and feed the data from the file path 
+    data = pd.read_csv('C:/Users/nston/Desktop/MachineLearning/Project 1/Glass Data/glass.data')
+    mas = len(data)
+    print(mas)
+    for j in range(mas): 
+        print(data.iloc[j])
+        print('\n')
+    
+    for i in data: 
+        print(i)
+    #print(data.head())
+
+
 
     print("Program Finish")
 
