a6db968867040935a7b1d46530dea02f5ea514f4
Nicholas Stone <stonen2@xavier.edu>
2020-08-25 19:55:16 -0600

    Changing the structure and cleaning Data

--- Vote_Data.py
+++ Vote_Data.py
@@ -1,12 +1,14 @@
-import pandas
+import pandas as pd
 import os 
+
+
 
 
 
 class Vote:
 
     def PreProcess(): 
-        print("Hello World!")
+        DataFrame = pd.read_csv('')
 
     def __init__(): 
         PreProcess()
