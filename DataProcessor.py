a47f4c6af36e1f8af6fe9574a116bd0f062195c5
matteobjornsson <matteobjornsson@gmail.com>
2020-08-27 20:01:37 -0600

    dataprocessor changes

--- DataProcessor.py
+++ DataProcessor.py
@@ -14,7 +14,9 @@ class DataProcessor:
 
     def fix_missing_attrs(self, df: pd.DataFrame) -> pd.DataFrame:
         # if only small percent of examples have missing attributes, remove those examples.
+            # i.e. check rowwise, calculate percentage
         # if only a small fraction of columns (e.g. 2/12) have missing attributes, remove those columns. 
+            # i.e. check columnwise, calculate percentage
         # if many datapoints across many columns have missing attributes, generate at random to match column distribution. 
         #   find attribute value distribution across discrete options (find min/max?) Use pandas stats for this
         return df
@@ -22,15 +24,14 @@ class DataProcessor:
     def has_continuous_values(self, df: pd.DataFrame) -> bool:
         for col in df:
             # if number of unique values is greater than threshold, consider column continuous-valued
-            # print(col, '\n', df[col].nunique(), '\n')
             if df[col].nunique() > self.discrete_threshold:
                 return True
         return False
 
     def discretize(self, df: pd.DataFrame) -> pd.DataFrame:
         for col in df:
-            if df[col].nunique() > self.discrete_threshold:
-                # take the difference between the min value and max value, split all values into @bin_count bins
+            if df[col].nunique() > self.discrete_threshold and col != "class":
+                # split all values into @bin_count bins
                 df[col] = pd.cut(df[col], bins=self.bin_count, labels=[*range(0,self.bin_count,1)])
         return df
 
@@ -40,6 +41,7 @@ if __name__ == '__main__':
     print(df.head())
     dp = DataProcessor()
     if dp.has_continuous_values(df):
+        print("Attribute values continuous, discretizing...\n")
         df = dp.discretize(df)
     print(df.head())
     
