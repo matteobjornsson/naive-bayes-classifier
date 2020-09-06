08c0ca9e83456105aba7f671598803ce3efb403c
matteobjornsson <matteobjornsson@gmail.com>
2020-09-05 19:06:07 -0600

    loss function file

---
+++ Results.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+"""
+loss functions 
+
+use 0/1 loss
+https://stats.stackexchange.com/questions/296014/why-is-the-naive-bayes-classifier-optimal-for-0-1-loss
+0/1 loss penalizes missclassification
+
+
+"""
