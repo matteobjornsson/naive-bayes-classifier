b918e408355975f3659c783adb7759faa789e1f4
matteobjornsson <matteobjornsson@gmail.com>
2020-09-06 01:22:59 -0600

    rename ML2, debug calculateF

--- TrainingAlgorithm.py
+++ TrainingAlgorithm.py
@@ -3,8 +3,9 @@ import numpy as np
 import random 
 import sys 
 import copy 
+import pprint
 
-class Training_Algorithm:
+class TrainingAlgorithm:
 
     def __init__(self):
         self.attr = []
@@ -89,9 +90,11 @@ class Training_Algorithm:
     # take in dataset and calculate occurence of each class
     def calculateN(self, df: pd.DataFrame) -> dict:
         n = {}
-        Class = len(df.columns)
+        print(df.head)
+        ClassIndex = len(df.columns)-1
+        print("class index: ", ClassIndex)
         for i in range(len(df)):
-            ClassValue = df.iloc[Class][i] 
+            ClassValue = df.iloc[i][ClassIndex] 
             if ClassValue in n: 
                n[ClassValue] += 1 
                continue
@@ -109,32 +112,32 @@ class Training_Algorithm:
         return QValue
 
     def calculateF(self, n: dict, df: pd.DataFrame) -> dict:
-        FMatrix = {}
-        count = 0 
-        for col in df.columns: 
-            if count == len(df.columns): 
-                break
-            count+=1 
+        fMatrix = {}
+        print(df.head)
+        columnList = list(df.columns.values)[:-1]
+        for col in columnList: 
             for row in range(len(df)): 
-                AttributeValue = df.iloc[col][row]
-                ClassValue = df.iloc[len(df.columns)][row]
-                if ClassValue in Fmatrix.keys(): 
-                    if col in ClassValue.keys():
-                        if AttributeValue in col.keys(): 
-                            Fmatrix[ClassValue][col][AttributeValue] +=1 
+                AttributeValue = df.at[row, col]
+                ClassValue = df.iloc[row][len(df.columns)-1]
+
+                if ClassValue in fMatrix.keys(): 
+                    if col in fMatrix[ClassValue].keys():
+                        if AttributeValue in fMatrix[ClassValue][col].keys(): 
+                            fMatrix[ClassValue][col][AttributeValue] +=1 
                         else: 
-                            Fmatrix[ClassValue][col][AttributeValue] = 1 
+                            fMatrix[ClassValue][col][AttributeValue] = 1 
                     else: 
-                        Fmatrix[ClassValue][col] = {AttributeValue:1}
+                        fMatrix[ClassValue][col] = {AttributeValue:1}
                 else: 
-                    Fmatrix[ClassValue] = {col:{AttributeValue:1}}
-        for k in Fmatrix.keys(): 
-            for ClassI in ClassValue.keys(): 
-                for AttributeValueI in AttributeValue.keys(): 
-                    Value = Fmatrix[k][ClassI][AttributeValueI] 
+                    fMatrix[ClassValue] = {col:{AttributeValue:1}}
+       
+        for ClassValueI in fMatrix.keys():
+            for Attribute in fMatrix[ClassValueI].keys(): 
+                for AttributeValue in fMatrix[ClassValueI][Attribute].keys(): 
+                    Value = fMatrix[ClassValueI][Attribute][AttributeValue] 
                     Value +=1 
-                    Fmatrix[k][ClassI][AttributeValueI]  = (Value/(n[ClassI] + len(df.columns-1)))
-        return Fmatrix
+                    fMatrix[ClassValueI][Attribute][AttributeValue]  = (Value/(n[ClassValueI] + (len(df.columns)-1)))
+        return fMatrix
                
                 
                 
@@ -149,35 +152,42 @@ class Training_Algorithm:
         # iterate over all values in nested dict
             # add 1 and divide by the count of examples in the class (n[class]) plus the total number of examples
             # i.e. (v + 1)/(n[class] + d)
-        return f
 
 if __name__ == '__main__':
     print("Program Start")
     filename = sys.argv[1]
     df = pd.read_csv(filename)
-    ta = Training_Algorithm() 
-    te = ta.CrossValidation(df)
-    for i in range(len(te)): 
-        print("LENGTHS: ")
-        print(len(te[i]))
-    test = ta.BinTestData(df)
-    print("=================")
-    for i in range(len(test)): 
-        print("LENGTHS")
-        print(len(test[i]))
-    df1 = ta.ShuffleData(df)
+    # ta = Training_Algorithm() 
+    # te = ta.CrossValidation(df)
+    # for i in range(len(te)): 
+    #     print("LENGTHS: ")
+    #     print(len(te[i]))
+    # test = ta.BinTestData(df)
+    # print("=================")
+    # for i in range(len(test)): 
+    #     print("LENGTHS")
+    #     print(len(test[i]))
+    # df1 = ta.ShuffleData(df)
 
     #df2 = pd.DataFrame(df1)
-    count = 0 
-    for i in range(len(df)): 
-        for j in range(len(df.columns)):
+    # count = 0 
+    # for i in range(len(df)): 
+    #     for j in range(len(df.columns)):
            
-            #print(df2.iloc[i][j])
-            if df.iloc[i][j] != df1.iloc[i][j]:
-                print("CHANGE")
-                count+=1 
-    print(count)
-    print(len(df1))
-    
+    #         #print(df2.iloc[i][j])
+    #         if df.iloc[i][j] != df1.iloc[i][j]:
+    #             print("CHANGE")
+    #             count+=1 
+    # print(count)
+    ta = TrainingAlgorithm()
+    n = ta.calculateN(df)
+    print("n: ", n)
+
+    q = ta.calculateQ(n, len(df))
+    print("q: ", q)
+
+    f = ta.calculateF(n, df)
+    print("f: \n")
+    pprint.pprint(f)
 
     print("Program Finish")
