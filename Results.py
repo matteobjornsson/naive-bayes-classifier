fab40baa3e60240b9d86147663c205b1861093c3
Nicholas Stone <stonen2@xavier.edu>
2020-09-05 21:50:21 -0600

    Pushing Up shell for

--- Results.py
+++ Results.py
@@ -34,21 +34,40 @@ class Results:
                 self.ClassificationCorrect.append(df.iloc[i][ClassificationHypothesis])
             else: 
                 self.ClassificationWrong.append(df.iloc[i][ClassificationHypothesis]
-        #TotalTestSet = len(self.ClassificationCorrect) + len(self.ClassificationWrong)
-        #TotalCorrect = (len(self.ClassificationCorrect) / TotalTestSet) * 100 
-        TotalWrong = (len(self.ClassificationWrong) / TotalTestSet) * 100 
+        TotalTestSet = len(self.ClassificationCorrect) + len(self.ClassificationWrong)
+        TotalCorrect = (len(self.ClassificationCorrect) / TotalTestSet) * 100 
+        #TotalWrong = (len(self.ClassificationWrong) / TotalTestSet) * 100 
         Statistics = list() 
-        #Statistics.append(TotalTestSet)
-        #Statistics.append(TotalCorrect)
-        Statistics.append(TotalWrong)
+        Statistics.append(TotalTestSet)
+        Statistics.append(TotalCorrect)
+        #Statistics.append(TotalWrong)
         return Statistics
 
 
+    """
+    https://towardsdatascience.com/multi-class-metrics-made-simple-part-i-precision-and-recall-9250280bddc2
+https://towardsdatascience.com/multi-class-metrics-made-simple-part-i-precision-and-recall-9250280bddc2
+
+    
+    """
+
+
     def F1FunctionBins(self,df:pd.DataFrame): 
+        UniqueClassifiers = list() 
+        Classes = len(df.columns)-1
+        for i in range(len(df)): 
+            if df.iloc[i][Classes] in UnqiqueClassifiers: 
+                continue 
+            UniqueClassifiers.append(df.iloc[i][Classes])
+            continue 
+        
+
         ClassificationHypothesis = len(df.columns)
         TrueClassification = len(df.columns -1)
         if df.iloc[i][ClassificationHypothesis] == df.iloc[i][TrueClassification]: 
+            #If they are the same then a false positive 
 
+        
 
 
 if __name__ == '__main__':
