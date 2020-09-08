e7b1f62e27fe20ff71c6e501b50c14195c8336e4
Nicholas Stone <stonen2@xavier.edu>
2020-09-07 20:21:45 -0600

    Pushing up the experiment

--- Driver.py
+++ Driver.py
@@ -16,14 +16,16 @@ import DataProcessor
 import pprint
 
 
-#Parameters: A list, a Dataframe and an Integer 
+#Parameters: Dataset name, A list, a Dataframe and an Integer 
 #Return: None
 #Function: Take in the Result data, The data frame of data, and the trial number and print to a file 
-def WriteToAFile(Results,DataFrame,Trial):
+def WriteToAFile(Setname, Results,Trial):
     #Set the file name based on the trial number 
     FileName = "Naive Bayes Results " + str(Trial) + ".csv"
     #Open the file in write mode 
     f = open(FileName, "w")
+    #Append the data set name to the csv files 
+    f.write(str(Setname) + "\n")
     #For each of the results in the list 
     for i in Results: 
         #Write with a new line character
@@ -33,7 +35,16 @@ def WriteToAFile(Results,DataFrame,Trial):
     #Close the file 
     f.close()
 
-
+def Average(TotalRunCount, Stats ) -> list: 
+    f1score = 0 
+    ZOloss = 0 
+    for i in range(len(Stats)): 
+        if i % 2 == 0: 
+            ZOloss += Stats[i]
+        else: 
+            f1score += Stats[i]
+    f1score = f1score / TotalRunCount
+    ZOloss = ZOloss / TotalRunCount
 
 #Parameters: None
 #Return: None
@@ -58,82 +69,79 @@ def main():
     # SoybeanData = 'PreProcessedSoybean.csv'
     
     ####################################################### MACHINE LEARNING PROCESS #####################################################
+    AvgStats = list() 
+    TotalRun = 10 
     for dataset in data_sets:
-        df = pd.read_csv(dataset) 
-        #Return a clean dataframe with missing attributes taken care of 
-        # df = dp.StartProcess(df)
-        ML = TrainingAlgorithm.TrainingAlgorithm()
-        #Dataframe without noise Its a list of 10 mostly equal dataframes
-        NoNoiseDf = ML.BinTestData(df)
-        #DataFrame with Noise 
-        NoiseDf =  ML.ShuffleData(df)
-        #Return a list of 10 mostly equal sized dataframes
-        NoiseDf = ML.BinTestData(NoiseDf)
-        #Make One dataframe to hold all of the other Training dataframes 
-        TrainingDataFrame = pd.DataFrame()
-        #Make One dataframe that is our test Dataframe 
-        TestingDataFrame = NoNoiseDf.pop(TestData) 
-        for i in range(len(NoNoiseDf)):     
-            #Append the training dataframe to one dataframe to send to the ML algorithm 
-            TrainingDataFrame = TrainingDataFrame.append(NoNoiseDf[i], ignore_index=True)
-        
-        # print("Driver training data: \n", TrainingDataFrame)
-        # print("Driver testing dataframe: \n", TestingDataFrame)
+        for i in range(10): 
+            print(dataset)
+            df = pd.read_csv(dataset) 
+            #Return a clean dataframe with missing attributes taken care of 
+            # df = dp.StartProcess(df)
+            ML = TrainingAlgorithm.TrainingAlgorithm()
+            #Dataframe without noise Its a list of 10 mostly equal dataframes
+            NoNoiseDf = ML.BinTestData(df)
+            #DataFrame with Noise 
+            NoiseDf =  ML.ShuffleData(df)
+            #Return a list of 10 mostly equal sized dataframes
+            NoiseDf = ML.BinTestData(NoiseDf)
+            #Make One dataframe to hold all of the other Training dataframes 
+            TrainingDataFrame = pd.DataFrame()
+            #Make One dataframe that is our test Dataframe 
+            TestingDataFrame = NoNoiseDf.pop(TestData) 
+            for i in range(len(NoNoiseDf)):     
+                #Append the training dataframe to one dataframe to send to the ML algorithm 
+                TrainingDataFrame = TrainingDataFrame.append(NoNoiseDf[i], ignore_index=True)
+            
+            # print("Driver training data: \n", TrainingDataFrame)
+            # print("Driver testing dataframe: \n", TestingDataFrame)
 
 
-        #Calculate the N value for the Training set
-        TrainingN = ML.calculateN(TrainingDataFrame)
-        #Calculate the Q value for the Training set
-        TrainingQ = ML.calculateQ(TrainingN,len(TrainingDataFrame))
-        #Calculate the F Matrix for the Training set
-        TrainingF = ML.calculateF(TrainingN,TrainingDataFrame)
-        # print("N: \n")
-        # pprint.pprint(TrainingN)
-        # print("Q: \n")
-        # pprint.pprint(TrainingQ)
-        # print("F: \n")
-        # pprint.pprint(TrainingF)
+            #Calculate the N value for the Training set
+            TrainingN = ML.calculateN(TrainingDataFrame)
+            #Calculate the Q value for the Training set
+            TrainingQ = ML.calculateQ(TrainingN,len(TrainingDataFrame))
+            #Calculate the F Matrix for the Training set
+            TrainingF = ML.calculateF(TrainingN,TrainingDataFrame)
 
-        #Create a Classifier Object to classify our test set 
-        model = Classifier.Classifier(TrainingN,TrainingQ,TrainingF)
-        #Reassign the testing dataframe to the dataframe that has our Machine learning classification guesses implemented 
-        TestingDataFrame = model.classify(TestingDataFrame)
-        
-        # print("Classified test set: \n")
-        # print(TestingDataFrame)
 
-        #Get some statistics on the Machine learning 
-        #Create a Results object
-        Analysis = Results.Results()
+            #Create a Classifier Object to classify our test set 
+            model = Classifier.Classifier(TrainingN,TrainingQ,TrainingF)
+            #Reassign the testing dataframe to the dataframe that has our Machine learning classification guesses implemented 
+            TestingDataFrame = model.classify(TestingDataFrame)
+            
+            # print("Classified test set: \n")
+            # print(TestingDataFrame)
 
-        #List to hold our stats
-        Stats = list()  
-        #Run the 0/1 Loss function on our results
-        zeroOne = Analysis.ZeroOneLossFunctionStats(TestingDataFrame)
-        #Run the stats summary on our results 
-        classStats, microStats, macroStats = Analysis.statsSummary(TestingDataFrame)
-        Stats.append(classStats)
-        Stats.append(microStats)
-        Stats.append(macroStats)   
-        print("Zero one loss: \n")
-        print(zeroOne)
-        print("F1 Matrix score: \n")
-        print(
-            "per-class stats: \n", classStats, 
-            '\n micro-Averaged stats: \n', microStats, 
-            '\n macro-Averaged stats: \n', macroStats
-            )
+            #Get some statistics on the Machine learning 
+            #Create a Results object
+            Analysis = Results.Results()
 
-        # #Send the Data to a csv file for human checking and hyper parameter tuning 
-        WriteToAFile(Stats, TestingDataFrame,Trial)
-        Trial += 1 
-        TestData +=1 
-        if TestData == 10: 
-            TestData = 0 
+            #List to hold our stats
+            Stats = list()  
+            #Run the 0/1 Loss function on our results
+            zeroOne = Analysis.ZeroOneLossFunctionStats(TestingDataFrame)
+            #Run the stats summary on our results 
+            classStats, microStats, macroStats = Analysis.statsSummary(TestingDataFrame)
+            print("Zero one loss: \n")
+            print(zeroOne)
+            print("F1 Matrix score: \n")
+            print(
+                "per-class stats: \n", classStats, 
+                '\n micro-Averaged stats: \n', microStats, 
+                '\n macro-Averaged stats: \n', macroStats
+                )
+            AvgStats.append(macroStats["macroF1"])
+            AvgStats.append(zeroOne)
+            
+            # #Send the Data to a csv file for human checking and hyper parameter tuning 
+            
+            Trial += 1 
+            TestData +=1 
+            if TestData == 10: 
+                TestData = 0 
     # #Increment the Trial and Testdata Number and do it again 
-    # Trial+=1 
-    # TestData +=1
-
+        AvgStats = Average(TotalRun,AvgStats)
+        WriteToAFile(dataset, AvgStats,Trial)
     print("Program Finish")
 
 
